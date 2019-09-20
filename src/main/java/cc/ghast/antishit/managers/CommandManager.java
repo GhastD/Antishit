72e784f9222af4b2b89ee298cddf87851571eb62
Thibaut J <shanyujuneja@gmail.com>
2019-09-20 21:25:38 +0200

    Forgot to init the main command

--- src/main/java/cc/ghast/antishit/managers/CommandManager.java
+++ src/main/java/cc/ghast/antishit/managers/CommandManager.java
@@ -2,11 +2,12 @@ package cc.ghast.antishit.managers;
 
 
 import cc.ghast.antishit.api.manager.Manager;
+import cc.ghast.antishit.commands.ShitCommand;
 
 public class CommandManager extends Manager {
 
     public void init(){
-
+        new ShitCommand();
     }
 
     public void disinit(){
