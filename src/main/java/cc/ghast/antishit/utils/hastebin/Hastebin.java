3c5f4d553d4cc51501e1fca01077431b1dc2fb20
Thibaut J <shanyujuneja@gmail.com>
2019-09-22 19:51:02 +0200

    Added hastebin tool util
    Still needs to be implemented

---
+++ src/main/java/cc/ghast/antishit/utils/hastebin/Hastebin.java
@@ -0,0 +1,35 @@
+package cc.ghast.antishit.utils.hastebin;
+
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URL;
+
+public class Hastebin {
+
+
+    private static final String endpoint = "https://hasteb.in/documents";
+
+    public static String paste(final String[] s, final String s2) throws IOException {
+        String payload = "";
+        for (String string : s){
+            payload = payload + "\n" + string;
+        }
+        HTTPRequest request = new HTTPRequest(new URL(endpoint));
+        request.setTimeout(1000);
+        request.setUseragent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36");
+        request.setPostData(payload);
+        String[] res;
+        try {
+            res = request.read();
+            String reply = res[0];
+            JsonParser parser = new JsonParser();
+            return "https://hasteb.in/" + parser.parse(reply).getAsJsonObject().get("key").getAsString();
+        } catch (Exception e){
+            System.out.println("[!]---> Unhandled HTTP request exception");
+
+        }
+        return null;
+    }
+}
